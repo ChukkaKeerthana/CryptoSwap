06342962d079e4ecc3628e625c6a64e39720bbca
Keerthana <chukkakeerthana2019@gmail.com>
2023-05-08 12:46:10 +0530

    myApp

---
+++ my_app/lib/utils/constants.dart
@@ -0,0 +1,15 @@
+String infura_url =
+    "https://goerli.infura.io/v3/509dfef1ccd844f6ad3f3f2577e47566";
+
+String sepolia_infura_url =
+    "https://sepolia.infura.io/v3/509dfef1ccd844f6ad3f3f2577e47566";
+
+String owner_private_key =
+    "7723b33e8b65ae810418f900829eb0a3b44dad4591bd3df4078994382fbf508e"; //ENTER YOUR PRIVATE KEY
+
+String currentPriceAddress = "0xa629704d23831C1dc811B5d48828BEBd15745385";
+
+String swapTokenAddress =
+    "0xF0722754Ee2f26C9614Ba293D5beB5ec0C8AE919"; //ENTER YOUR TOKENSWAP CONTRACT ADDRESS OR INCREASE ALLOWANCE.
+
+String getBalanceContractAddress = "0xb1f64E85e1762da11067634945fAa83AD5A45689";
